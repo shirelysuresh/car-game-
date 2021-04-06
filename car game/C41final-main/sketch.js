ffc8c016dbd0ddcdcc9676f15162b19b54567ca0
shirelysuresh <77004136+shirelysuresh@users.noreply.github.com>
2021-04-06 10:15:18 +0530

    Add files via upload

---
+++ car game/C41final-main/sketch.js
@@ -0,0 +1,76 @@
+var canvas, backgroundImage;
+
+var gameState = 0,finishedPlayers;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+var track, car1_img, car2_img, car3_img, car4_img;
+
+function preload(){
+  track = loadImage("images/track.jpg");
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+  ground = loadImage("images/ground.png");
+  f1=loadImage ("images/f1.png");
+  carSound=loadSound("sound/car.mp3")
+  sliding=loadSound("sound/sliding.mp3")
+  goldImg=loadImage("images/gold.png")
+  silverImg=loadImage("images/silver.png")
+  bronzeImg=loadImage("images/bronze.png")
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth , displayHeight);
+  database = firebase.database();
+  gameState = 0;
+  distance = 0;
+ // finishedPlayers = 0;
+  yVel = 0;
+  xVel = 0;
+
+  xSet = false;
+  game = new Game();
+  game.getState();
+  game.start();
+
+obstacles=createGroup();
+for(i=0;i<7;i++){
+  a=random(200,950)
+
+  b=random(-height*4,height-300)
+
+  oilspill=createSprite(a,b);
+  oilspill.addImage("f1",f1)
+  obstacles.add(oilspill)
+
+}
+
+
+}
+
+
+function draw(){
+   //start the game
+   background(200, 200, 255);
+
+   //start the game
+   if (playerCount === 4 ) {
+     game.update(1);
+   }
+ 
+   //start the game for real
+   if (gameState === 1) {
+     game.play();
+   }
+   if (gameState === 2) {
+     console.log("End");
+   }}
+ 
+  
